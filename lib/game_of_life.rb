ad2d2af8d471b07d153892eb182cd67f344420de
Jorge <jvargas18@ucol.mx>
2021-02-05 13:34:39 -0600

    Add GameOfLife class Add tests for the GameOfLife class that contains all the logic and rules and also add the class,

---
+++ lib/game_of_life.rb
@@ -0,0 +1,54 @@
+require "matrix"
+require "prime"
+require_relative "cell.rb"
+require_relative "board.rb"
+
+class GameOfLive
+  def initialize(width, height)
+    @rows = height
+    @cols = width
+    @matrix_cells = Matrix.build(height, width) { Cell.new(rand(0..1) == 1) }
+    @board = Board.new(width, height)
+  end
+
+  def evaluate_life(matrix)
+    matrix_cells_next = Matrix.build(@rows, @cols) { Cell.new }
+    (0...@rows).each do |row|
+      (0...@cols).each do |col|
+        state = matrix[row, col].state
+        neighbors = count_neighbors(matrix, row, col)
+        matrix_cells_next[row, col].state = eveluate_rules(state, neighbors)
+      end
+    end
+    matrix_cells_next
+  end
+
+  def eveluate_rules(cell_state, neighbors)
+    return true if !cell_state && neighbors == 3 # Birth
+    return false if cell_state && neighbors < 2 || neighbors > 3 # Die
+
+    cell_state # surive
+  end
+
+  def count_neighbors(matrix, row, col)
+    sum = 0
+    (-1..1).each do |i|
+      (-1..1).each do |j|
+        sum += matrix[(row + i + @rows) % @rows, (col + j + @cols) % @cols].state ? 1 : 0
+      end
+    end
+    sum -= (matrix[row, col].state ? 1 : 0)
+    sum
+  end
+
+  def play
+    loop do
+      sleep(0.5)
+      @matrix_cells = evaluate_life(@matrix_cells)
+      @board.draw(@matrix_cells)
+    end
+  end
+end
+
+# game = GameOfLive.new(40, 60)
+# game.play
---
+++ spec/game_of_life_spec.rb
@@ -0,0 +1,44 @@
+require_relative "../lib/game_of_life.rb"
+require_relative "../lib/cell.rb"
+
+RSpec.describe "Game of life" do
+  let(:game_of_life) { GameOfLive.new(3, 3) }
+  let(:cell) { Cell.new }
+  let(:matrix) { Matrix.build(3, 3) { Cell.new } }
+
+  it "a cell has 3 live neighbors" do
+    matrix[0, 0].state = true
+    matrix[1, 0].state = true
+    matrix[2, 0].state = true
+    expect(game_of_life.count_neighbors(matrix, 1, 1)).to eq 3
+  end
+
+  it "a dead cell with 3 neighbors born in the next generation" do
+    neighbors = 3
+    expect(game_of_life.eveluate_rules(cell.state, neighbors)).to be true
+  end
+
+  it "a living cell with 1 neighbor or less die in the next generation" do
+    neighbors = 1
+    cell.state = true
+    expect(game_of_life.eveluate_rules(cell.state, neighbors)).to be false
+  end
+
+  it "a living cell with 4 or more neighbors die in the next generation" do
+    neighbors = 4
+    cell.state = true
+    expect(game_of_life.eveluate_rules(cell.state, neighbors)).to be false
+  end
+
+  it "a living cell with 2 neighbors remain alive in the next generation" do
+    neighbors = 2
+    cell.state = true
+    expect(game_of_life.eveluate_rules(cell.state, neighbors)).to be true
+  end
+
+  it "a living cell with 2 neighbors remain alive in the next generation" do
+    neighbors = 3
+    cell.state = true
+    expect(game_of_life.eveluate_rules(cell.state, neighbors)).to be true
+  end
+end
