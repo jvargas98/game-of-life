e934338cb46854834b7ea29022961fb52c847cad
Jorge <jvargas18@ucol.mx>
2021-02-05 13:34:27 -0600

    Change tests descriptions

--- spec/board.rb
+++ spec/board.rb
@@ -3,11 +3,11 @@ require "matrix"
 require_relative "../lib/board.rb"
 require_relative "../lib/cell.rb"
 
-RSpec.describe "Cell" do
+RSpec.describe "Board" do
   let(:board) { Board.new(2, 2) }
   let(:matrix) { Matrix.build(2, 2) { Cell.new } }
 
-  it "print . ■ ■ . two cell alive" do
+  it "print . ■ ■ . two cells alive" do
     matrix[0, 1].state = true
     matrix[1, 0].state = true
     expect do
