6b42191d1bfb09c19f8cb00ceb670ccf3bfb6101
Jorge <jvargas18@ucol.mx>
2021-02-05 03:39:36 -0600

    Change the board size's order

--- spec/board.rb
+++ spec/board.rb
@@ -1,12 +1,15 @@
 require "byebug"
+require "matrix"
 require_relative "../lib/board.rb"
 require_relative "../lib/cell.rb"
 
 RSpec.describe "Cell" do
   let(:board) { Board.new(2, 2) }
-  let(:matrix) { [[Cell.new, Cell.new(true)], [Cell.new(true), Cell.new()]] }
+  let(:matrix) { Matrix.build(2, 2) { Cell.new } }
 
-  it "print . ■ ■ . two cell is alive" do
+  it "print . ■ ■ . two cell alive" do
+    matrix[0, 1].state = true
+    matrix[1, 0].state = true
     expect do
       board.draw(matrix)
     end.to output(". ■ \n■ . \n").to_stdout
